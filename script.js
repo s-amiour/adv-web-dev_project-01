d9204411e67f371d8eb8c8733c4a6703e744d1bb
Sultan Amiour <sultan.amiour@epita.fr>
2025-09-16 15:50:19 +0200

    feat.: burger menu functionality

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // select the toggle button
-const toggleBtn = document.querySelector(".header__toggle-btn")
+const toggleBtn = document.querySelector(".header__toggle-btn");
 // select the header
-const header = document.querySelector(".header")
+const header = document.querySelector(".header");
 
 toggleBtn.addEventListener("click", () => {
 	// add or remove the "header-open" class from the header element
 	// write your code here
-	console.log("run the function here !!")
+	header.classList.toggle("header-open");
 })
